3243c767a3473910161826d1e444c955e5b020d4
Лавров Артем Евгеньевич <Lavrov1@WUD.lan>
2021-04-05 11:14:50 +0300

    Первое задание

--- SentencesParserTask.cs
+++ SentencesParserTask.cs
@@ -1,4 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
 
 namespace TextAnalysis
 {
@@ -6,9 +9,19 @@ namespace TextAnalysis
     {
         public static List<List<string>> ParseSentences(string text)
         {
+            var stopWords = new string[] { "Mr.", "Mrs." };
             var sentencesList = new List<List<string>>();
-            //...
-            return sentencesList;
+            var punctuation = new char[] { '.', '!', '?', ';', ':', '(', ')' };
+            var sentences = text
+                .Split(punctuation).Except(stopWords)
+                .Select(a => a.Split(' ', ',', '"',)
+                .Where(b => b.Any(c => char.IsLetter(c) && char.IsLower(b[0])
+                || c == '`' && char.IsLetter(c) && char.IsLower(b[0])))
+                .Select(b => b).ToList())
+                .Where(z => z.Count != 0).Select(z => z).ToList();
+
+
+            return sentences;
         }
     }
 }
